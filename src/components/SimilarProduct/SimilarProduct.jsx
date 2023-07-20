3a851a90e51eb012442046db670717879d0d9d21
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2023-07-20 21:10:29 +0200

    SimilarProduct Actualizar

--- src/components/SimilarProduct/SimilarProduct.jsx
+++ src/components/SimilarProduct/SimilarProduct.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styles from './SimilarProduct.css';
-import sidra from '../../assets/PhotoBox/sidra.png'; 
+import sidra from '../../assets/PhotoBox/sidra.png';
+import pulpo from '../../assets/PhotoBox/pulpo.png';
+import cecina from '../../assets/PhotoBox/cecina.png';
+import paloSanto from '../../assets/PhotoBox/palo-santo.png';
 
 function SimilarProduct() {
   return (
-    <div className={styles.container}>
-      <div className={styles.photoBox}>
-        <img src={sidra} alt="sidra" />
+    <div >
+      <div className={styles.container}>
+        <div className={styles.photoBox}>
+          <img src={sidra} alt="sidra" />
+        </div>
+        <p>Sidra</p>
+
+        <div className="photoBox">
+          <img src={pulpo} alt="pulpo" />
+        </div>
+        <p>pulpo</p>
+
+        <div className="photoBox">
+          <img src={cecina} alt="cecina" />
+        </div>
+        <p>cecina</p>
+
+        <div className="photoBox">
+          <img src={paloSanto} alt="palo-santo" />
+        </div>
+        <p>palo santo</p>
       </div>
-      <p>Sidra</p>
     </div>
+   
   );
 }
 
