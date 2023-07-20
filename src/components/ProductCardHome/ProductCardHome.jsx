cac942daaec965258a9703061bb498ccf6a7d791
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2023-07-20 13:05:57 +0200

    Cards con API

--- src/components/ProductCardHome/ProductCardHome.jsx
+++ src/components/ProductCardHome/ProductCardHome.jsx
@@ -2,50 +2,69 @@
 import { FaShoppingCart } from '@react-icons/all-files/fa/FaShoppingCart';
 import "./ProductCardHome.css";
 import { AiFillHeart } from '@react-icons/all-files/ai/AiFillHeart';
+import React, { useState, useEffect } from 'react';
 
 
+function ProductCardHome() {
 
-function ProductCardHome(props) {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchProducts() {
+          try {
+            // Fetch the list of products from your API
+            let res = await fetch("http://localhost:3000/products");
+            let json = await res.json();
+            setProducts(json);
+          } catch (err) {
+            // Handle errors if any
+            console.error("Error fetching products:", err);
+          }
+        }
+
+        fetchProducts();
+    }, []);
+
     return(
-        <div className='productList'>
-            <div key={props.id} className='productCard'>
-                <img src="public/aceiteOlive-img.jpg" alt="product-img" className='productImage' />
-            </div>
-
-            <div className='productCard_content'>
-
-                <h3 className='productName'>Aceite de Oliva Virgen Extra Premium</h3>
-            
-                <div className='origin-details'>
-                    <div className='store'>
-                        <p>Proveniencia</p>
-                        <span className="store">Olivares de Jaén</span>
+        <div>
+            {products.map((product) => (
+                <div className='productList'>
+                    <div key={product.id} className='productCard'>
+                        <img src="public/aceiteOlive-img.jpg" alt="product-img" className='productImage' />
                     </div>
 
-                    <div className='origin'>
-                        <p>Orígen</p>
-                        <span className="origin">Olivares de Jaén</span>
+                    <div className='productCard_content'>
+
+                        <h3 className='productName'>{product.name}</h3>
+                    
+                        <div className='origin-details'>
+                            <div className='store'>
+                                <p>Proveniencia</p>
+                                <span className="store">{product.store}</span>
+                            </div>
+
+                            <div className='origin'>
+                                <p>Orígen</p>
+                                <span className="origin">{product.origin}</span>
+                            </div>
+                        </div>
+
+                        <div className="description">
+                            <p>{product.description}</p>
+                        </div>
+                    </div>
+                    <div className="details-shop">   
+                        <AiFillHeart className='heart'/>
+                        <div className="buy">
+                            <span>Comprar</span> 
+                        <FaShoppingCart className='cart' />
+                        </div> 
+
+                        <div className="productPrice">{product.price}</div>               
                     </div>
                 </div>
-
-                <div className="description">
-                    <p>Aceite de Oliva Virgen Extra de categoría superior, obtenido directamente de aceitunas cuidadosamente seleccionadas y recolectadas a mano en olivares centenarios de Jaén, Andalucía. El resultado es un aceite de oliva de color verde brillante, con aromas frutados y un sabor equilibrado entre notas amargas y picantes. Cada botella de este Aceite de Oliva Virgen Extra Premium es el fruto del arduo trabajo y conocimiento de los agricultores y productores artesanos de Olivares de Jaén.</p>
-                </div>
-            </div>
-            <div className="details-shop">   
-                <AiFillHeart className='heart'/>
-                <div className="buy">
-                    <span>Comprar</span> 
-                   <FaShoppingCart className='cart' />
-                </div> 
-
-                <div className="productPrice">12.5 €</div>
-            
-               
-            </div>
+            ))}
         </div>
-        
-
     )
 
 
