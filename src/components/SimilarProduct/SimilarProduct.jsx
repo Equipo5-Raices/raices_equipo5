7c013032eb25fa8e0f298a6d9069717767796810
Minerva1922 <minervapv83@gmail.com>
2023-07-20 18:50:15 +0200

    SimilarProduct construyendo

---
+++ src/components/SimilarProduct/SimilarProduct.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './SimilarProduct.css';
+import sidra from '../../assets/PhotoBox/sidra.png'; 
+
+function SimilarProduct() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.photoBox}>
+        <img src={sidra} alt="sidra" />
+      </div>
+      <p>Sidra</p>
+    </div>
+  );
+}
+
+export {SimilarProduct}
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProductCard from "../../components/ProductCard/ProductCard"; 
-
+import SimilarProduct from '../../components/SimilarProduct/SimilarProduct';
 export const ProductPage = () => {
   return (
     <div>
       <ProductCard />
+      <SimilarProduct />
     </div>
   );
 };
+export  {ProductPage};
