b1b22f68ed38ddb1bad79951169bc74895fd8783
Juan Antonio <56176388+JUANAN2019@users.noreply.github.com>
2023-07-21 10:46:14 +0200

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,36 +3,31 @@ import { Card, Container, Row, Col } from "react-bootstrap";
 import styles from "./ProductCard.module.css";
 
 import Aceite from "../../assets/aceite.jpeg";
+let url = window.location;
+let idd=url.pathname.slice(-1);
 
 const ProductCard = () => {
-  const [product, setProduct] = useState(null);
+  
+  const [products, setProducts] = useState([]);
+    
 
-  useEffect(() => {
-    const getProduct = async () => {
-      try {
-        let options = {
-          method: "GET",
-          headers: {
-            "Content-type": "application/json; charset=utf-8",
-          },
-        };
-        const res = await fetch("http://localhost:3000/products/2", options);
-        const data = await res.json();
-        setProduct(data);
-      } catch (err) {
-        console.error("Error fetching product:", err);
-      }
-    };
-
-    getProduct();
-  }, []);
-
-  if (!product) {
-    return <div>Loading...</div>;
-  }
-
+  
+  
+  
+    useEffect(() => {
+      fetch(`https://127.0.0.1:8000/api/products/${idd}`)
+        .then(response => response.json())
+        .then(data => setProducts(data))
+        
+        .catch(error => console.log('error', error));
+    }, [products]);
+ 
   return (
-    <Card key={product._id} className={`${styles["card-container"]}`}>
+    <>
+  
+
+    
+    <Card key={products.id} className={`${styles["card-container"]}`}>
       <Container>
         
         <Card.Img
@@ -44,7 +39,7 @@ const ProductCard = () => {
 
         <div className={styles["card-title-container"]}>
           <Card.Title className={styles["card-title"]}>
-            {product.name}
+            {products.name}
           </Card.Title>
         </div>
 
@@ -52,24 +47,23 @@ const ProductCard = () => {
           <Card.Text
             className={`${styles["text-start"]} ${styles["py-2"]} ${styles["card-text"]} ${styles["product-description"]}`}
           >
-            {product.description}
+            {products.description}
           </Card.Text>
 
           <Card.Text className={`${styles["card-text"]}`}>
-            {product.price}€
+            {products.price}€
           </Card.Text>
-          <div className={styles["small-text-caja"]}>
-             <Card.Text className={`{styles["small-text"]}`}>
-            <strong>{product.store}</strong>
+          <Card.Text className={`{styles["small-text"]}`}>
+            <strong>{products.store}</strong>
           </Card.Text>
           <Card.Text className={`${styles["small-text"]} `}>
-            {product.origin}
+            {products.origin}
           </Card.Text>
-          </div>
-         
         </div>
       </Container>
     </Card>
+     
+    </>
   );
 };
 
